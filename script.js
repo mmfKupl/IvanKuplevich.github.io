ba4063ed4d2757865b5a849e0256cace5fbadcf7
mmfKupl <ikuplevich@mail.ru>
2019-05-25 11:57:43 +0300

    adaptived for mobile

--- script.js
+++ script.js
@@ -97,6 +97,18 @@ if (!window.navigator.userAgent.includes("Mobile")) {
     var h5p = dataProject.querySelector("h5 + p");
     dataProject.querySelector(".place-role").insertBefore(p, h5p);
   });
+
+  var header = document.querySelector("header");
+  var main = document.querySelector("main");
+  main.style.marginTop = header.offsetHeight + "px";
+
+  var swapSections = document.querySelectorAll(".section-mob-swap");
+  console.log(swapSections);
+  swapSections.forEach(el => {
+    var dateS = el.querySelector(".section-date");
+    var roleS = el.querySelector(".place-role");
+    roleS.insertBefore(dateS, roleS.firstChild);
+  });
 }
 
 bDate.innerText = " " + getNumWithPrefix(getAge(), YEARS);
